b255b2c85637b8e01de9e3221ec9eeb600a2bf1b
ramesh kumar mahato <90126203+rameshsingh9813@users.noreply.github.com>
2021-12-21 10:53:13 +0545

    Update map_the_debris.js

--- map_the_debris.js
+++ map_the_debris.js
@@ -1,9 +1,41 @@
- function orbitalPeriod(arr) {
-     const GM = 398600.4418;
-     const earthRadius = 6367.4447;
-     let g=arr.map(({name:name,avgAlt:orbitalPeriod})=>({name,orbitalPeriod}))
-     let h=g.map((e)=>e.orbitalPeriod=(Math.round(2*Math.PI*Math.sqrt(Math.pow((earthRadius+e.orbitalPeriod),3)/GM))))
-return g
-    }
-  
-console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]))
+
+const Person = function(firstAndLast) {
+    // Only change code below this line
+    // Complete the method below and implement the others similarly
+    this.setFullName = function(x) {
+        firstAndLast=x;
+        return firstAndLast;
+    };
+    this.setFirstName = function(x) {
+        let fal=firstAndLast.split(' ')
+        let sfn=x.split(' ');
+        let retsfn=sfn[0]+" "+fal[1];
+        firstAndLast=retsfn;
+        return  firstAndLast
+    };
+    this.setLastName = function(x) {
+        let fal=firstAndLast.split(' ')
+        let retsfn=fal[0]+" "+x;
+        firstAndLast=retsfn;
+        return  firstAndLast
+    };
+    this.getFullName = function() {
+      return firstAndLast ;
+    };
+    this.getFirstName = function() {
+      let k=firstAndLast.split(' ')
+      k=k[0]
+      return k ;
+    };
+    this.getLastName = function() {
+        let k=firstAndLast.split(' ')
+        k=k[1]
+        return k ;
+    };
+  };
+  const bob = new Person('Bob Ross');
+  bob.setFirstName("Haskell")
+  bob.setFirstName("ramesh")
+  bob.setLastName("kumar mahato")
+
+  console.log(bob.getFullName())
